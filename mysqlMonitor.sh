8c8e62caded3b8268a307a30d23cdb2fb07a4254
Chris Kimberley <chris.kimberley@gmail.com>
2015-06-19 16:57:04 -0400

    Testing
    Testing with Kuru

--- mysqlMonitor.sh
+++ mysqlMonitor.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+#Testing comment
 
 #This script currently checks for slave status, and possibly for MySQL Enterprise Backup status.  
 #Currently it expects that any hostnames ending in 04 are running MySQL Enterprise Backup.  If yes, the monitor looks at the backup tables.
